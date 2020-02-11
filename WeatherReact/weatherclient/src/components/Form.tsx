ff788563a9635306feb65f3ec3791148faadca74
Sergei Severin <sseverin@rihousing.com>
2020-02-11 15:06:20 -0500

    added Form and WeatherDetails components and used them in Home component

---
+++ WeatherReact/weatherclient/src/components/Form.tsx
@@ -0,0 +1,13 @@
+﻿import React from 'react'
+class Form extends React.Component {
+    render() {
+        return (
+            <div>
+                <input id="country" type="text" name="country" placeholder=" Countrys..." />
+                <input id="city" type="text" name="city" placeholder=" Citys..."/>
+            </div>
+        );
+
+    }
+}
+export default Form;
